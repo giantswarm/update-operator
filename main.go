a1f43d51b295bf441eb176fc128d5f610a4fa38b
Tim Schindler <tim@giantswarm.io>
2019-12-17 10:10:04 +0100

    rename template-operator to update-operator (#1)

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 	"github.com/giantswarm/versionbundle"
 	"github.com/spf13/viper"
 
-	"github.com/giantswarm/template-operator/flag"
-	"github.com/giantswarm/template-operator/pkg/project"
-	"github.com/giantswarm/template-operator/server"
-	"github.com/giantswarm/template-operator/service"
+	"github.com/giantswarm/update-operator/flag"
+	"github.com/giantswarm/update-operator/pkg/project"
+	"github.com/giantswarm/update-operator/server"
+	"github.com/giantswarm/update-operator/service"
 )
 
 var (
